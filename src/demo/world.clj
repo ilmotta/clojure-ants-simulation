7230f6c1ae89e8cd8ca4ee62a02d60860368447c
ilmotta <icaro.ldm@gmail.com>
2017-02-03 00:41:00 -0200

    Simplify delta location function

--- src/demo/world.clj
+++ src/demo/world.clj
@@ -3,26 +3,17 @@
             [demo.store :as store]
             [demo.util :refer [bound]]))
 
-;; dirs are 0-7, starting at north and going clockwise these are the deltas in
-;; order to move one step in given dir.
-(def ^:private dir-delta
-  {0 [0 -1]
-   1 [1 -1]
-   2 [1 0]
-   3 [1 1]
-   4 [0 1]
-   5 [-1 1]
-   6 [-1 0]
-   7 [-1 -1]})
+(def ^:private direction-delta
+  {0 [0 -1], 1 [1 -1], 2 [1 0], 3 [1 1] 4 [0 1], 5 [-1 1], 6 [-1 0], 7 [-1 -1]})
 
 (def ^:private home-locations
   (doall (for [x (config :home-range) y (config :home-range)] [x y])))
 
-(defn ^:private delta-loc
-  "Returns the location one step in the given dir. Note the world is a torus."
-  [[x y] direction]
-  (let [[dx dy] (dir-delta (bound 8 direction))]
-    [(bound (config :dim) (+ x dx)) (bound (config :dim) (+ y dy))]))
+(def ^:private bound-location
+  (partial map (partial bound (config :dim))))
+
+(defn ^:private delta-location [location direction]
+  (->> direction (bound 8) direction-delta (map + location) bound-location))
 
 (defn ^:private rand-location [_]
   ((juxt rand-int rand-int) (config :dim)))
@@ -33,13 +24,13 @@
 (defn home-places []
   (map store/place home-locations))
 
-(defn nearby-places [location direction]
-  (->> (map #(% direction) [identity dec inc])
-       (map (partial delta-loc location))
-       (map store/place)))
-
 (defn evaporate [place]
   (update place :pher * (config :evaporation-rate)))
+
+(defn nearby-places [location direction]
+  (->> (map #(% direction) [identity dec inc])
+       (map (partial delta-location location))
+       (map store/place)))
 
 (def ant? (comp boolean :ant))
 (def available? (comp not :ant))
